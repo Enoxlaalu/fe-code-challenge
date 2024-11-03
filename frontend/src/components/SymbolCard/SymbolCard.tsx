f2caad15757150a771d5637c4a706c6d913ca77c
Enoxlaalu <enoxlaalu@gmail.com>
2024-11-04 06:51:46 +0700

    put shadow and effect states in separate custom hooks

--- frontend/src/components/SymbolCard/SymbolCard.tsx
+++ frontend/src/components/SymbolCard/SymbolCard.tsx
@@ -2,8 +2,10 @@ import './symbolCard.css';
 import { useAppSelector } from '@/hooks/redux';
 import SymbolCardHeader from '@/components/SymbolCard/SymbolCardHeader';
 import SymbolCardBody from '@/components/SymbolCard/SymbolCardBody';
-import { memo, useRef } from 'react';
+import { memo, useEffect, useRef } from 'react';
 import { selectShowCardInfo } from '@/store/dashboardOptionsSlice';
+import useAddBoxShadow from '@/hooks/useAddBoxShadow';
+import useAddEffect from '@/hooks/useAddEffect';
 
 type SymbolCardProps = {
   id: string;
@@ -14,6 +16,9 @@ type SymbolCardProps = {
 
 const SymbolCard = memo(({ id, onClick, price, activeSymbol }: SymbolCardProps) => {
   const ref = useRef<HTMLDivElement>(null);
+  const ref2 = useRef(price);
+  const { shadow, addShadow, setShadow } = useAddBoxShadow();
+  const { effect, addEffect, setEffect } = useAddEffect();
   const { trend, companyName, industry, marketCap } = useAppSelector(
     (state) => state.stocks.entities[id]
   );
@@ -23,33 +28,45 @@ const SymbolCard = memo(({ id, onClick, price, activeSymbol }: SymbolCardProps) 
     onClick(id);
   };
 
+  useEffect(() => {
+    addShadow(price, ref2.current);
+  }, [price]);
+
+  useEffect(() => {
+    addEffect(price, ref2.current);
+    ref2.current = price;
+  }, [activeSymbol, price]);
+
   const getClassName = () => {
     let className = 'symbolCard';
 
     if (activeSymbol) {
-      if (activeSymbol === id) className = className + ' symbolCard--active';
-      if (activeSymbol !== id) className = className + ' symbolCard--nonactive';
+      if (activeSymbol === id) {
+        className = className + ` symbolCard--active`;
+      } else className = className + ` symbolCard--nonactive`;
     }
 
-    const prevPrice = Number(ref.current?.dataset.price);
-    const increasePercent = (100 * (price - prevPrice)) / prevPrice;
-
-    if (price > prevPrice) {
-      if (increasePercent >= 25) {
-        className = className + ' symbolCard--shake';
-      }
-
-      className = className + ' symbolCard--green';
-    }
-    if (price < Number(ref.current?.dataset.price)) {
-      className = className + ' symbolCard--red';
-    }
+    if (effect) className = className + ` symbolCard--${effect}`;
+    if (shadow) className = className + ` symbolCard--${shadow}`;
 
     return className;
   };
 
+  const onAnimationEnd = () => {
+    if (ref.current) {
+      setShadow('');
+      setEffect('');
+    }
+  };
+
   return (
-    <div ref={ref} data-price={price} onClick={handleOnClick} className={getClassName()}>
+    <div
+      ref={ref}
+      data-price={price}
+      onClick={handleOnClick}
+      className={getClassName()}
+      onAnimationEnd={onAnimationEnd}
+    >
       <SymbolCardHeader id={id} trend={trend} />
       <SymbolCardBody
         companyName={companyName}
